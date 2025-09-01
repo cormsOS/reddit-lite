46b1e7eb95c822ee1447afbcc71d7a5d58ee97aa
Cormac O'Sullivan <cormac.osullivan@sainsburys.co.uk>
2025-09-01 15:40:24 +0100

    feat: add site to github

---
+++ src/app/search/page.tsx
@@ -0,0 +1,24 @@
+import PostList from "@/components/posts/post-list";
+import { fetchPostsBySearchTerm } from "@/db/queries/posts";
+import { redirect } from "next/navigation";
+
+interface SearchPageProps {
+  searchParams: Promise<{
+    term: string;
+  }>;
+}
+
+export default async function SearchPage({ searchParams }: SearchPageProps) {
+  const { term } = await searchParams;
+
+  if (!term) {
+    redirect(`/`);
+  }
+
+  return (
+    <div>
+      <h1 className="mb-4 text-xl">Search Results for: {term}</h1>
+      <PostList fetchData={() => fetchPostsBySearchTerm(term)} />
+    </div>
+  );
+}
