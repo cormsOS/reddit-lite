d4eeb110cc979182d7dd13f45a2d2fe1eee31a21
Cormac O'Sullivan <cormac.osullivan@sainsburys.co.uk>
2025-09-01 17:23:08 +0100

    refactor: copilot redesign

--- src/components/comments/comment-create-form.tsx
+++ src/components/comments/comment-create-form.tsx
@@ -58,9 +58,9 @@ export default function CommentCreateForm({
 
   return (
     <div>
-      <Button size="sm" variant="light" onClick={() => setOpen(!open)}>
+      {open ? null : <Button size="sm" variant="light" onClick={() => setOpen(!open)}>
         Reply
-      </Button>
+      </Button>}
       {open && form}
     </div>
   );
